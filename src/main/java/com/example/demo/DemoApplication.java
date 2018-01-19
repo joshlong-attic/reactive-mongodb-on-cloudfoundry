927ab51a6354bc9ce3a54354bbc74447a6ab9979
Josh Long <starbuxman@gmail.com>
2018-01-19 05:05:16 -1000

    initial commit of an example that uses Spring Boot 2.0.0.M7 and reactive MongoDB on Pivotal Web Services (based on Cloud Foundry).

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,59 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import reactor.core.publisher.Flux;
+
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	@Bean
+	RouterFunction<?> routes() {
+		return route(RequestPredicates.GET("/hi"), request -> ok().body(Flux.just("Hello, world"), String.class));
+	}
+
+	@Bean
+	ApplicationRunner run(PersonRepository repository) {
+		return args ->
+				repository
+						.deleteAll()
+						.thenMany(
+								Flux.just("A", "B", "C", "D")
+										.map(x -> new Person(null, x))
+										.flatMap(repository::save))
+						.thenMany(repository.findAll())
+						.subscribe(System.out::println);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
+
+interface PersonRepository extends ReactiveMongoRepository<Person, String> {
+}
+
+@Data
+@Document
+@AllArgsConstructor
+@NoArgsConstructor
+class Person {
+
+	@Id
+	private String id;
+
+	private String name;
+}
